2f93a8b91a4d8aaac0943e666859c248d6aa5f72
vpalmerio <vpalmerio@protonmail.com>
2023-11-28 16:33:59 -0500

    Modified constructor to take owner address as parameter

--- contracts/ElecToken.sol
+++ contracts/ElecToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ElecToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("ElecToken", "ELC") {
-        _mint(msg.sender, initialSupply);
+    constructor(uint256 initialSupply, address contractOwner) ERC20("ElecToken", "ELC") {
+        _mint(contractOwner, initialSupply);
     }
 }
